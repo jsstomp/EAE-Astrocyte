f07843f4e312d68191e7bea13c7d36635eedd185
jsstomp <jaftastomp@gmail.com>
2018-02-27 11:27:06 +0100

    Added filtering method option, and midway commandline input if user wants pca plot, plus some cleaner code

--- Code/PCA_plotter.R
+++ Code/PCA_plotter.R
@@ -1,7 +1,6 @@
 ####################################################################
-# original author: M. Dubbelaar
-# Date: 11-dec-2017
-# Edited by: Jafta Stomp  (15-02-2018)
+# Author: Jafta Stomp
+# Date: 27-02-2018
 # Description: 
 #   This script filters out lowly expressed genes using DAFS.R
 #   and also creates PCA plots to look at if the different conditions
@@ -11,17 +10,19 @@
 ####################################################################
 #                            IMPORTS                               #
 ####################################################################
-suppressMessages(library(DESeq2))
-suppressMessages(library(edgeR))
+cat("Importing necessary libraries...\n")
 suppressMessages(library(ggplot2))
 suppressMessages(library(RColorBrewer))
 
 ####################################################################
 #                           FUNCTIONS                              #
 ####################################################################
-cbPalette <- c('#a6cee3','#b2df8a','#fb9a99','#fdbf6f','#ff7f00','#b15928',
-               '#cab2d6','#1f78b4','#6a3d9a','#33a02c','#e31a1c','#696969')
+cat("Building functions...\n")
+cbPalette <- c('#1f78b4','#e31a1c','#33a02c','#696969','#b2df8a','#fdbf6f',
+               '#ff7f00','#b15928','#a6cee3','#cab2d6','#fb9a99','#6a3d9a')
 
+# Function to be called for building PCA plots of AEA astrocyte data.
+# Uses PCdata and changes color depending on data input.
 PCAplot <- function(data, PC1, PC2, color, subtitle, percent_var, colname){
   gg <- ggplot(data, aes(PC1, PC2, color=color)) +
     geom_point(size=5) +
@@ -29,23 +30,22 @@ PCAplot <- function(data, PC1, PC2, color, subtitle, percent_var, colname){
     labs(title="PCA plot", subtitle=subtitle, x=paste0("PC1: ",percent_var[1],"% variance"),
          y=paste("PC2: ",percent_var[2],"% variance")) +
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-          panel.background = element_blank(), axis.line = element_line(color = "black"))
+          panel.background = element_blank(), axis.line = element_line(color = "black")) + 
+    scale_color_manual(values=cbPalette, name = colname)
   col <- colnames(data[which(colnames(data)==color)])
-  if(colname=="groupID" | colname=="Date"){
-    gg <- gg + scale_color_manual(values=cbPalette)
-  }
-  else if(colname=="RIN" | colname=="RNA_input"){
+  if(colname=="RIN" | colname=="RNA_input"){
     gg <- gg + scale_colour_gradient(low = "red", high = "green")
   }
   else if(colname=="Score" | colname=="Input_rate") {
     gg <- gg + scale_colour_gradient(low = "green", high = "red")
   }
-  try(gg)
+  gg
 }
 
 ####################################################################
 #                    Load Necessary information                    #
 ####################################################################
+cat("Loading necessary information...\n")
 target[,1] <- factor(toupper(as.character(target[,1])))
 target$groupID <- paste(target$Condition, target$Region, target$Population, sep="_")
 target <- target[match(colnames(countData), target$Sample.ID.Marissa),]
@@ -61,10 +61,11 @@ cd_SCA <- countData[,which(colnames(countData) %in% target_SCA$Sample.ID.Marissa
 ####################################################################
 #                          Preprocess Data                         #
 ####################################################################
+cat("Preprocessing data...\n")
 target$groupID <- factor(target$groupID) 
 dds <- DESeqDataSetFromMatrix(as.matrix(countData), target, ~groupID)
 dds <- dds[ rowSums(counts(dds)) > 1, ]
-
+# Make dds objects for the sub datasets as well.
 dds_HBA <- DESeqDataSetFromMatrix(cd_HBA, target_HBA, ~groupID)
 dds_HBA <- dds_HBA[ rowSums(counts(dds_HBA)) > 1, ]
 dds_HBAG <- DESeqDataSetFromMatrix(cd_HBAG, target_HBAG, ~groupID)
@@ -74,13 +75,13 @@ dds_SCA <- dds_SCA[ rowSums(counts(dds_SCA)) > 1, ]
 ####################################################################
 #                             PCA plots                            #
 ####################################################################
-# rlog transformation, may take some time, so please be patient.
-#rld <- rlog(dds, blind=FALSE)
+# variance stabilizing transformation,  instead of rlog to save some time, and the quality stays high.
 rld <- vst(dds)
 vsd_HBA <- vst(dds_HBA)
 vsd_HBAG <- vst(dds_HBAG)
 vsd_SCA <- vst(dds_SCA)
 
+# Create PCA data for the large dataset
 data_ACSA <- plotPCA(rld, intgroup=c("groupID"), returnData=TRUE)
 data_ACSA$Region <- target$Region
 data_ACSA$Population <- target$Population
@@ -88,20 +89,18 @@ data_ACSA$Condition <- target$Condition
 data_ACSA$SampleID <- target$Sample.ID.Seq
 percentVar <- round(100 * attr(data_ACSA, "percentVar"))
 
-dataBuilder <- function(name){
-  temp <- paste("data_", name, sep="")
-  print(temp)
-  assign(temp, "test") 
-}
-
+# Initiate PCA data building for all sub datasets.
 data_HBA <- plotPCA(vsd_HBA, intgroup=c("groupID"), returnData=TRUE)
 data_HBAG <- plotPCA(vsd_HBAG, intgroup=c("groupID"), returnData=TRUE)
 data_SCA <- plotPCA(vsd_SCA, intgroup=c("groupID"), returnData=TRUE)
 
-count <- 0
+count <- 0  #initiates count to be used in coming for loop
 df_list <- list(HBA=data_HBA, HBAG=data_HBAG, SCA=data_SCA)
+# Start building PCA data for all subsets using a for loop per subset in df_list
+cat("Building PCA data structure...\n")
 for(i in df_list) {
-  count = count + 1
+  count = count + 1  #add +1 to count to represent the current index of the df_list
+  # Use assign to make percentVar and add collumns using within per dataset.
   assign(paste("percentVar_", names(df_list)[count], sep=""), round(100 * attr(get(paste("data_", names(df_list)[count], sep="")), "percentVar")))
   assign(paste("data_", names(df_list)[count], sep=""), within(i, {
     SampleID = get(paste("target_", names(df_list)[count], sep=""))$Sample.ID.Seq
@@ -113,27 +112,23 @@ for(i in df_list) {
   }))
 }
 
-# plot Population, Condition and Region
 pdf(paste("Results/PCA_", prefix, ".pdf", sep = ""))
+# plot Population, Condition, Region and GroupID
+cat("Creating plots Population, Condition, Region and GroupID...\n")
 lapply(names(data_ACSA[c(6:8,4)]), function(x) {
   plots <- PCAplot(data_ACSA, PC1, PC2, data_ACSA[x][[1]], paste("Astrocytes by", x), percentVar, x)
   print(plots)
 })
-  
-dev.off()
+
 # plot per region_population
-
 df_list <- list(data_HBA=data_HBA, data_HBAG=data_HBAG, data_SCA=data_SCA)
-
 count <- 0
-pdf(paste("Results/PCA_regPos_", prefix, ".pdf", sep = ""))
 for(i in df_list){
   count = count + 1
+  cat(paste("Creating plots for", gsub("data_","",names(df_list)[count]), "...\n", sep=""))
   plots <- lapply(colnames(i[c(4,10,9,8,7,6)]), function(x){
-    #print(class(i[x][[1]]))
     PCAplot(i, PC1, PC2, i[x][[1]], paste(gsub("data_", "", names(df_list)[count]), "astrocytes by", x), unlist(lapply(paste("percentVar_",gsub("data_","", names(df_list)), sep=""), get)[count]), x)
     })
   print(plots)
 }
 dev.off()
-
--- Code/prepare_expression.R
+++ Code/prepare_expression.R
@@ -1,7 +1,6 @@
 ####################################################################
-# original author: M. Dubbelaar
-# Date: 11-dec-2017
-# Edited by: Jafta Stomp  (15-02-2018)
+# Author: Jafta Stomp
+# Date: 27-02-2018
 # Description: 
 #   This script filters out lowly expressed genes using DAFS.R
 #   and also creates PCA plots to look at if the different conditions
@@ -9,16 +8,25 @@
 ####################################################################
 args = commandArgs(trailingOnly=TRUE)
 # test if there is at least one argument: if not, return an error
-if (length(args)==0) {
-  stop("At least one argument must be supplied (prefix).n", call.=FALSE)
-} else if (length(args)==1) {
+if (length(args)<2) {
+  stop("At least one argument must be supplied (prefix) (filtering method){DAFS|Smith}.n", call.=FALSE)
+} else if (length(args)==2) {
   prefix <- args[1]
+  method.filter <- toupper(args[2])
+} else if (length(args)>2){
+  stop("Too many arguments, pleas only supply (prefix) (filtering method){DAFS|Smith}.n", call.=FALSE)
 }
-#prefix <- "EAE2"
+
+if(method.filter=="DAFS"|method.filter=="SMITH"){
+  cat("R script prepare_expression.R has been succesfully initiated.\n")
+} else {
+  stop("Filtering method should either be DAFS or Smith")
+}
 
 ####################################################################
 #                            IMPORTS                               #
 ####################################################################
+cat("Importing necessary libraries...\n")
 suppressMessages(library(DESeq2))
 suppressMessages(library(edgeR))
 suppressMessages(library(ggplot2))
@@ -26,39 +34,45 @@ suppressMessages(library(RColorBrewer))
 suppressMessages(library(kimisc))
 
 # set working directory to parent parent working directory of this file
-setwd(gsub("prepare_expression.R","",thisfile()))
+suppressMessages(setwd(gsub("prepare_expression.R","",thisfile())))
 setwd("../..")
 
-#source("Code/DAFS.R")
+source("Experimental-autoimmune-encephalomyelitis-Astrocyte-RNA-seq-analysis/Code/DAFS.R")
 
 ####################################################################
 #                              Code                                #
 ####################################################################
+cat("Loading count data...\n")
 # Load count data of all samples and all genes
 countData <- read.table("RawFiles/mergedCounts.txt", header = T, row.names = 1, check.names = F)
 
-#if(!file.exists(paste("Results/filtered_counts_", prefix, ".txt", sep=""))){
-#  createDAFSfile(countData, prefix)
-#}
-
-#mat <- read.table(paste("Results/filtered_counts_", prefix, ".txt", sep=""), header = T, check.names = F)
-#mat_mlcpm <- aveLogCPM(mat)
-
-### Other filtering method, https://support.bioconductor.org/p/75914/ (Gordon Smith)
-M <- median(colSums(countData)) * 1e-6 # median library size (millions)
-idx <- rowSums( cpm(countData) >= 5/M ) >= ncol(countData)/2 
-nr_features <- length(which(idx == TRUE))
-countData <- countData[idx, ]
+# Use if statements to see which filtering method should be used
+if(method.filter=="DAFS"){
+  if(!file.exists(paste("Results/filtered_counts_", prefix, ".txt", sep=""))){
+    createDAFSfile(countData, prefix)
+  }
+  test <- readline("What is the preferred filtering method, DAFS or Smith")
+  countData <- read.table(paste("Results/filtered_counts_", prefix, ".txt", sep=""), header = T, check.names = F)
+} else if(method.filter=="SMITH"){
+  ### Other filtering method, https://support.bioconductor.org/p/75914/ (Gordon Smith)
+  cat("Filtering count data...\n")
+  M <- median(colSums(countData)) * 1e-6 # median library size (millions)
+  idx <- rowSums( cpm(countData) >= 5/M ) >= ncol(countData)/2 
+  nr_features <- length(which(idx == TRUE))
+  countData <- countData[idx, ]
+}
 mlcpm <- aveLogCPM(countData)
 
 # Information about the different samples is written into the target
 # file, the step loads all of the necessary information.
+cat("Loading phenotype data...\n")
 target <- read.table("RawFiles//target.csv", header = T, sep=",")
 ph_data <- read.table("RawFiles//180219_Sample\ info.csv", header = T, sep=",")
 colnames(ph_data)[2] <- "Sample.ID.Seq"
 target <- merge(target, ph_data[,c(2,4,6,10:12)], by="Sample.ID.Seq")
 
 # load file containing a list of samples to be excluded based on quality control
+cat("Excluding low quality samples...\n")
 exclude <- read.table("Results/excludes.txt")[,3]
 # exclude these samples
 target <- target[-which(target$Sample.ID.Marissa %in% exclude),]
@@ -67,19 +81,34 @@ target <- droplevels(target)
 
 countData <- countData[,which(colnames(countData) %in% target$Sample.ID.Marissa)]
 
-source("Experimental-autoimmune-encephalomyelitis-Astrocyte-RNA-seq-analysis/Code/PCA_plotter.R")
+PCA_check <- ""
+while(PCA_check!="y"|PCA_check!="n"){
+  con <- file("stdin")
+  cat("Would you like to get some PCA plots? [y/n]: ")
+  PCA_check <- readLines(con,1)
+  close(con)
+  if(PCA_check=="y"){
+    oldw <- getOption("warn")
+    options(warn = -1)
+    cat("Initiating PCA_plotter.R from source...\n")
+    suppressMessages(source("Experimental-autoimmune-encephalomyelitis-Astrocyte-RNA-seq-analysis/Code/PCA_plotter.R"))
+    options(warn = oldw)
+    break
+  } else if(PCA_check=="n"){
+    break
+  }
+}
 
 # Write results
-write.table(countData,"Results/count_data_NEW.txt",quote=F,col.names=NA,row.names=T, sep="\t")
-write.table(target,"Results/col_data_NEW.txt",quote=F,col.names=T,row.names=T, sep="\t")
+cat("Writing results...\n")
+write.table(countData,paste("Results/count_data_",prefix,".txt",sep=""),quote=F,col.names=NA,row.names=T, sep="\t")
+write.table(target,"Results/col_data.txt",quote=F,col.names=T,row.names=T, sep="\t")
 
+# Density plot of filtered counts
+cat("Create density plot of filtered counts.\n")
 pdf(paste("Results/Density_", prefix, ".pdf", sep = ""))
 ggplot() + aes(mlcpm) +
   geom_histogram(binwidth=0.2, colour = "tomato4", fill="tomato3") +
   labs(title="Filter Lowly Expressed Features", subtitle = "Densityplot of the average log CPM after filtering genes with less than 5 counts in half of the samples", x = "logCPM", y = "Frequency") +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(color = "black"))
-#ggplot() + aes(mat_mlcpm) +
- # geom_histogram(binwidth=0.2, colour = "tomato4", fill="tomato3") +
- #labs(title="Filter Lowly Expressed Features", subtitle = "Densityplot of the average log CPM after DAFS filtering", x = "logCPM", y = "Frequency") +
- #theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(color = "black"))
 dev.off()
