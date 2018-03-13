28eeda09acd381fd67c6a420f683d3779e476c90
jsstomp <jaftastomp@gmail.com>
2018-03-13 11:35:19 +0100

    Finished cleaning up venn_DEA.R, added comments and print statements, can now be fully run from commandline with needed arguments (one thing is that count file argument must be absolute path)

--- Code/venn_DEA.R
+++ Code/venn_DEA.R
@@ -13,8 +13,8 @@ if (length(args)<4) {
 } else if (length(args)==4) {
   prefix <- args[1]
   countfile <- args[2]
-  FDR <- args[3]
-  logFC <- args[4]
+  FDR <- as.numeric(args[3])
+  logFC <- as.numeric(args[4])
 } else if (length(args)>4){
   stop("Too many arguments, please only supply (prefix) (countfile) (FDR) (logFC).n", call.=FALSE)
 }
@@ -22,6 +22,7 @@ if (length(args)<4) {
 ####################################################################
 #                           IMPORTS                                #
 ####################################################################
+cat("Loading required libraries.\n")
 suppressMessages(library(DESeq2))
 suppressMessages(library(edgeR))
 suppressMessages(library(ggplot2))
@@ -35,29 +36,36 @@ suppressMessages(setwd(gsub("venn_DEA.R","",thisfile())))
 setwd("../..")
 
 # load count and col data
+cat("Loading expression and phenotype data.\n")
 countData <- read.table(countfile, header = T, check.names = F)
 ph_data <- read.table("Results/col_data.txt", header = T, row.names = 4, check.names = F)
 colnames(countData) <- rownames(ph_data)
-colData <- ph_data[,4:6]
+colData <- ph_data[,4:6]  #only interesting columns
 colData$Group <- paste(colData$Condition, colData$Region, colData$Population, sep="_")
 
 ####################################################################
 #                           FUNCTIONS                              #
 ####################################################################
 
+# venn_DEA is a function that does DEA using DESeq2 and return a dataframe of FDR and logFC
 venn_DEA <- function(group1, group2) {
   columns <- colData[which(colData$Group == group1 | colData$Group == group2),]
   counts <- countData[,which(colnames(countData) %in% rownames(columns))]
   columns$Group <- as.factor(columns$Group)
   
+  # actual DE analysis starts here
   dds <- DESeqDataSetFromMatrix(counts,
                                 columns,
                                 design = ~ Group)
   dds <- DESeq(dds)
-  res <- results(dds, alpha=0.01, lfcThreshold = 2, pAdjustMethod = "BH")
+  res <- results(dds, alpha=FDR, lfcThreshold = logFC, pAdjustMethod = "BH")
   return(res[c(2,6)])
 }
 
+# vennDataBuilder is a function that calls on venn_DEA and compares the control of the given region with E1, E4 and Ech
+# saving the FDR and logFC and adding them to the dataframe of the given region. Then checks within the dataframe which
+# samples and genes have significantly de genes given the FDR and logFC thresholds given on the commandline.
+# Returns the dataframe
 vennDataBuilder <- function(db, region){
   # c-E1
   control = paste("C",region, sep="_")
@@ -70,13 +78,14 @@ vennDataBuilder <- function(db, region){
     db[paste("log2FoldChange_",gsub(paste("_",region,sep=""),"",x),sep="")] <- pl$log2FoldChange
   }
   db = within(db, {
-    E1.control = ifelse(log2FoldChange_E1 > logFC & padj_E1 < FDR, 1, 0)
-    E4.control = ifelse(log2FoldChange_E4 > logFC & padj_E4 < FDR, 1, 0)
-    Ech.control = ifelse(log2FoldChange_Ech > logFC & padj_Ech < FDR, 1, 0)
+    E1.control = ifelse(abs(log2FoldChange_E1) > logFC & padj_E1 < FDR, 1, 0)
+    E4.control = ifelse(abs(log2FoldChange_E4) > logFC & padj_E4 < FDR, 1, 0)
+    Ech.control = ifelse(abs(log2FoldChange_Ech) > logFC & padj_Ech < FDR, 1, 0)
   })
   return(db)
 }
 
+# venning is a function that creates a venn diagram given a dataframe. The diagram consist of three circles (E1,E4,Ech)
 venning <- function(db, title){
   a1 = nrow(subset(db, E1.control==1))
   a2 = nrow(subset(db, E4.control==1))
@@ -95,16 +104,24 @@ venning <- function(db, title){
 #                               CODE                               #
 ####################################################################
 
+# initiate the dataframes.
 HBA_db <- data.frame(row.names=rownames(countData))
 HBAG_db <- data.frame(row.names=rownames(countData))
 SCA_db <- data.frame(row.names=rownames(countData))
 
+# build dataframes using vennDataBuilder
+cat("Running differential expression analysis for HB_A. please wait...\n")
 suppressMessages(HBA_db <- vennDataBuilder(HBA_db,"HB_A"))
+cat("Running differential expression analysis for HB_AG. please wait...\n")
 suppressMessages(HBAG_db <- vennDataBuilder(HBAG_db, "HB_AG"))
+cat("Running differential expression analysis for SC_A. please wait...\n")
 suppressMessages(SCA_db <- vennDataBuilder(SCA_db, "SC_A"))
 
+# make vennDiagrams
+cat("Creating Venn Diagrams and writing to Results.\n")
 pdf(paste("Results/", prefix, "_VennDiagrams.pdf",sep=""))
 venning(HBA_db, "Region: Hindbrain acsa")
 venning(HBAG_db, "Region: Hindbrain acsa-glast")
 venning(SCA_db, "Region: Spinal cord acsa")
-dev.off()
+invisible(dev.off())
+cat("Done!\n")
